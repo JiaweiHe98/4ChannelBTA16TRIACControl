1be836e31e8fcf9aa9c57fc67947286489b3af38
JiaweiHe98 <45532786+JiaweiHe98@users.noreply.github.com>
2021-03-18 22:24:22 -0400

    Added threading to backend script

--- src/RaspberryPi/setting_update.py
+++ src/RaspberryPi/setting_update.py
@@ -6,6 +6,7 @@ A USB cable can be used for physically connecting the Arduino to Raspberry Pi
 
 # If not found, you need to install pyserial
 import serial
+import concurrent.futures
 import time
 
 # The first argument specify the serial port of the device
@@ -15,50 +16,73 @@ arduinoData = serial.Serial('COM6', 115200)
 # Power level initialization, power primarily goes from 0 to 1000
 power_set = [0, 0, 0, 0]
 
-# Reading settings from a file
-while True:
+# Flag
+isChanged = False
+lock = False
 
-    # Flag
-    isChanged = False
+# Update seetings function
+def update_settings():
+    # Reading settings from a file
+    while True:
 
-    # Read the light intensity from file
-    with open("lightIntensity.txt", "r") as light_intensity:
+        # Read the light intensity from file
+        with open("lightIntensity.txt", "r") as light_intensity:
 
-        # Read the light intensity from lines
-        light_levels = light_intensity.readlines()
-        
-        # Update light intensity for each channel in order
-        for i in range(4):
-
-            # Parse int from string
-            try:
-                light_level = int(light_levels[i])
-
-                if power_set[i] != light_level:
-                    power_set[i] = light_level
-                    isChanged = True
-
-            # Print out error signal
-            except:
-                print('input file error!')
-
+            # Read the light intensity from lines
+            light_levels = light_intensity.readlines()
             
-    # Update setting
-    if isChanged:
-        arduinoData.write(str(power_set).encode())
-        isChanged = False
+            # Update light intensity for each channel in order
+            for i in range(4):
 
-    # Decode message send back from Arduino
-    start = time.perf_counter()
-    dataFromArduino = arduinoData.readline().decode('ASCII')[:-2]
-    end = time.perf_counter()
+                # Parse int from string
+                try:
+                    light_level = int(light_levels[i])
 
-    # Convert string to list
-    power_read = eval(dataFromArduino)
+                    if power_set[i] != light_level:
+                        power_set[i] = light_level
+                        isChanged = True
 
-    # Check the power level of the Arduino and update the settings if the settings are different
-    if power_read != power_set:
-        arduinoData.write(str(power_set).encode())
+                # Print out error signal
+                except:
+                    print('input file error!')
 
-    # For debug
-    print(power_read, ' ', str(power_set).encode(), ' ', end - start, power_read != power_set)
+                
+        # Update setting
+        if isChanged:
+
+            while lock:
+                pass
+
+            lock = True
+            arduinoData.write(str(power_set).encode())
+            lock = False
+
+            isChanged = False
+            
+# Function for checking the settings and resend the settings
+def check_settings():
+
+    while True:
+        # Decode message send back from Arduino
+        dataFromArduino = arduinoData.readline().decode('ASCII')[:-2]
+
+        # Convert string to list
+        power_read = eval(dataFromArduino)
+
+        # Check the power level of the Arduino and update the settings if the settings are different
+        if power_read != power_set:
+
+            while lock:
+                pass
+
+            lock = True
+            arduinoData.write(str(power_set).encode())
+            lock = False
+
+
+if __name__ == "__main__":
+
+    # Running settings update and checking concurrently
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(update_settings)
+        executor.submit(check_settings)
